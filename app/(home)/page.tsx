f57c3ff15f8be347c73222fe3afec3251ce22d9b
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-03-31 17:39:53 -0500

    up SEO

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,10 @@
 import { MainProducts } from '@/components/Home/MainProducts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'Future Eccomerce',
+	description: 'Welcome to the future world ecommerce',
+};
 
 export default function Home() {
 	return (
